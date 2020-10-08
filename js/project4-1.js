cda58dfeb8fa5f0109b3e023e78b76f0b38d2525
kristinbunnell <kristinmbunnell@byui.edu>
2020-10-07 19:10:13 -0600

    javascript pulled from project 4-1

---
+++ js/project4-1.js
@@ -0,0 +1,15 @@
+function addNumbers()
+// INPUT
+let number1 = document.getElementById("number1").value;
+let number2 = document.getElementById("number2").value;
+
+// PROCESSING
+// convert the input strings to numbers using parseFloat;
+number1 = parseFloat(number1);
+number2 = parseFloat(number2);
+//add the two numbers together and store in my output variable;
+let sum = number1 + number2;
+
+// OUTPUT
+document.getElementById("output").textContent = sum;
+}
