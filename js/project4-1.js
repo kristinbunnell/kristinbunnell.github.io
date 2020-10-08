e732a58edefff40014fbb491724d04120ae57e15
kristinbunnell <kristinmbunnell@byui.edu>
2020-10-07 19:57:51 -0600

    updated

--- js/project4-1.js
+++ js/project4-1.js
@@ -4,7 +4,7 @@ let number1 = document.getElementById("number1").value;
 let number2 = document.getElementById("number2").value;
 
 // PROCESSING
-// convert the input strings to numbers using parseFloat;
+// convert the input strings to numbers by using parseFloat;
 number1 = parseFloat(number1);
 number2 = parseFloat(number2);
 //add the two numbers together and store in my output variable;
