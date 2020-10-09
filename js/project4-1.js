02e9bfb15f24fb48e5af05e34589ae4ae1a31d0b
kristinbunnell <kristinmbunnell@byui.edu>
2020-10-08 18:11:00 -0600

    added squigly braces

--- js/project4-1.js
+++ js/project4-1.js
@@ -1,4 +1,4 @@
-function addNumbers()
+function addNumbers(){
 // INPUT
 let number1 = document.getElementById("number1").value;
 let number2 = document.getElementById("number2").value;
@@ -12,3 +12,4 @@ let sum = number1 + number2;
 
 // OUTPUT
 document.getElementById("output").innerHTML = "The sum of " + number1 + " and " + number2 + " equals " + sum + "!";
+}
