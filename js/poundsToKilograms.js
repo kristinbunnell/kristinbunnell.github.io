48d2cf2960d17eeceeef386c833fab001ec2e617
kristinbunnell <kristinmbunnell@byui.edu>
2020-10-15 16:21:05 -0600

    fixed equation

--- js/poundsToKilograms.js
+++ js/poundsToKilograms.js
@@ -1,9 +1,9 @@
 function poundsToKilograms() {
     //  INPUT VALUE
-    let pounds = parseFloat(document.getElementById('pounds').value);
+    let pounds = parseFloat(document.getElementById("pounds").value);
     //  PROCESSING
     //  converting from pounds to kilograms
-    let kilograms = 0.453592
+    let kilograms = pounds * 0.453592;
     //  OUTPUT
-    document.getElementById('output').textContent = kilograms.toFixed(1) + "Kilograms";
+    document.getElementById("output").innerHTML = kilograms.toFixed(2) + "Kilograms";
 }
